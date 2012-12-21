32352d062eddbf08168849a27000fc3dcef1c33f
James Vanneman <jvannem@gmail.com>
2012-12-20 22:38:18 -0500

    prevents duplicates in request page

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -1,12 +1,14 @@
 $(function(){
   $(".swap_receivers").tokenInput("users.json", {
     crossDomain:false,
-    theme: "facebook"
+    theme: "facebook",
+    preventDuplicates: true
   });
 
   $(".swap_tournaments").tokenInput("tournaments.json", {
     crossDomain:false,
-    theme: "facebook"
+    theme: "facebook",
+    preventDuplicates: true
   });
 })
 
